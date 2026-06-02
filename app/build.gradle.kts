1bf49503fdd29aa2eba054977f17ab4e45811542
badalaryal11 <badal.aryal@gmail.com>
2026-06-02 12:58:38 +0545

    feat: enable ABI splits in build configuration to generate architecture-specific APKs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,15 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
+    splits {
+        abi {
+            isEnable = true
+            reset()
+            include("arm64-v8a", "armeabi-v7a", "x86_64")
+            isUniversalApk = true
+        }
+    }
 }
 
 dependencies {
